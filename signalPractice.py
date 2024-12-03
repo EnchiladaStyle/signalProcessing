72cc3356f99f176a3afd17c97fa1e078ef502602
EnchiladaStyle <jon19073@byui.edu>
2024-12-02 19:05:21 -0700

    added comments

--- signalPractice.py
+++ signalPractice.py
@@ -1,6 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# t is an array of evenly spaced times between 0 and 1 second. 
+# pulse is a pure sin wave that represents the signals sent by the radar.
+# echoes are the same signal as pulse but shifted over
+# noise is a list of random numbers for each datapoint in pulse
+# we add each element from the lists in their corresponding positions. This creates the final signal.
+
 fs = 200  # Sampling frequency
 t = np.linspace(0, 1, fs)  # 1 second
 pulse = np.sin(2 * np.pi * 50 * t)  # 1 kHz pulse
