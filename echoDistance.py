0b4fa534e22c5f9d46c742dda7668bfbe28f3ce7
EnchiladaStyle <jon19073@byui.edu>
2024-12-10 09:59:14 -0700

    imported some important stuff

--- echoDistance.py
+++ echoDistance.py
@@ -1,3 +1,10 @@
 #TODO
 
 #impliment code to determine the distance of an object using the echo in the signal
+
+from scipy.fftpack import fft
+from simulatedSignal import received_signal
+from simulatedSignal import t
+from simulatedSignal import fs
+import numpy as np
+import matplotlib.pyplot as plt
