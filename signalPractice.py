1ac65526b79b51f11163343ed8e63d6af5443d2b
EnchiladaStyle <jon19073@byui.edu>
2024-12-11 15:18:10 -0700

    imported some important stuff

---
+++ signalPractice.py
@@ -0,0 +1,6 @@
+from scipy.fftpack import fft
+from simulatedSignal import received_signal
+from simulatedSignal import t
+from simulatedSignal import fs
+import numpy as np
+import matplotlib.pyplot as plt
