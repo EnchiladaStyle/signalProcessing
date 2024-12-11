e4c545a1edad43c2120cc8e9ec0915dfafa9752d
EnchiladaStyle <jon19073@byui.edu>
2024-12-11 15:22:26 -0700

    started echo distance

--- echoDistance.py
+++ echoDistance.py
@@ -8,3 +8,6 @@ from simulatedSignal import t
 from simulatedSignal import fs
 import numpy as np
 import matplotlib.pyplot as plt
+
+echo_distances = [lag / fs * 343 for lag in peak_lags]
+print(f"Echo distances: {echo_distances}")
