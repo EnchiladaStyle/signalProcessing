27ffb16f8b5cf32999231336b033b0ea297b1829
EnchiladaStyle <jon19073@byui.edu>
2024-12-11 15:18:05 -0700

    imported some important stuff

--- signalNoiseRatio.py
+++ signalNoiseRatio.py
@@ -1,2 +1,9 @@
 #TODO
 #use the fast fourier transform to determine what is just noise and then filter it out.
+
+from scipy.fftpack import fft
+from simulatedSignal import received_signal
+from simulatedSignal import t
+from simulatedSignal import fs
+import numpy as np
+import matplotlib.pyplot as plt
