41b29c6a21c49658fb34a484efcb92010ecc98a9
EnchiladaStyle <jon19073@byui.edu>
2024-12-11 15:17:56 -0700

    imported some important stuff

--- noiseReducer.py
+++ noiseReducer.py
@@ -1,2 +1,9 @@
 #TODO
 # remove noise from the signal
+
+from scipy.fftpack import fft
+from simulatedSignal import received_signal
+from simulatedSignal import t
+from simulatedSignal import fs
+import numpy as np
+import matplotlib.pyplot as plt
