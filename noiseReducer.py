25473182a51cbac0932ef075186800d5315b8a08
EnchiladaStyle <jon19073@byui.edu>
2024-12-11 15:22:41 -0700

    started noise reducer

--- noiseReducer.py
+++ noiseReducer.py
@@ -7,3 +7,12 @@ from simulatedSignal import t
 from simulatedSignal import fs
 import numpy as np
 import matplotlib.pyplot as plt
+
+from scipy.signal import butter, filtfilt
+b, a = butter(4, 0.1, btype='low')  # 0.1 is normalized cutoff frequency
+denoised_signal = filtfilt(b, a, received_signal)
+plt.plot(t, denoised_signal)
+plt.title("Denoised Signal")
+plt.xlabel("Time (s)")
+plt.ylabel("Amplitude")
+plt.show()
