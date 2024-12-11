6578ff05c58b3083fd5ab9b036a76c5a27948a9b
EnchiladaStyle <jon19073@byui.edu>
2024-12-11 15:22:18 -0700

    implemented cross correlation

--- crossCorrelation.py
+++ crossCorrelation.py
@@ -1,9 +1,17 @@
-# TODO
-# implement cross correlation of the signal
 
 from scipy.fftpack import fft
 from simulatedSignal import received_signal
 from simulatedSignal import t
 from simulatedSignal import fs
+from simulatedSignal import pulse
 import numpy as np
 import matplotlib.pyplot as plt
+
+from scipy.signal import correlate
+correlation = correlate(received_signal, pulse, mode='full')
+lags = np.arange(-len(received_signal)+1, len(received_signal))
+plt.plot(lags / fs, correlation)
+plt.title("Cross-Correlation")
+plt.xlabel("Lag (s)")
+plt.ylabel("Correlation")
+plt.show()
