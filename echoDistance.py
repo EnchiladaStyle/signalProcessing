7ba19bf12b9ffe3ad37062d04b568fa99468c4a9
EnchiladaStyle <jon19073@byui.edu>
2024-12-13 22:31:16 -0700

    cleaned up some code

--- echoDistance.py
+++ echoDistance.py
@@ -4,7 +4,6 @@
 
 from scipy.fftpack import fft
 from simulatedSignal import received_signal
-from simulatedSignal import t
 from simulatedSignal import fs
 import numpy as np
 import matplotlib.pyplot as plt
