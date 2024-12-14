7582893ff5533acbe33f35cf1785d3f715dee710
EnchiladaStyle <jon19073@byui.edu>
2024-12-13 22:31:59 -0700

    cleaned up some code

--- echoDistance.py
+++ echoDistance.py
@@ -6,7 +6,6 @@ from scipy.fftpack import fft
 from simulatedSignal import received_signal
 from simulatedSignal import fs
 import numpy as np
-import matplotlib.pyplot as plt
 
 echo_distances = [lag / fs * 343 for lag in peak_lags]
 print(f"Echo distances: {echo_distances}")
