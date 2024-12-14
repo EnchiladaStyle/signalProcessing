6cf95f493454d7f73d494bfc801f556d028df6df
EnchiladaStyle <jon19073@byui.edu>
2024-12-13 22:31:29 -0700

    cleaned up some code

--- signalPractice.py
+++ signalPractice.py
@@ -2,5 +2,4 @@ from scipy.fftpack import fft
 from simulatedSignal import received_signal
 from simulatedSignal import t
 from simulatedSignal import fs
-import numpy as np
 import matplotlib.pyplot as plt
