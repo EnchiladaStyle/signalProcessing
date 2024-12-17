c84a5dfbbc117e134a2757f632c9875f4f9e5740
EnchiladaStyle <jon19073@byui.edu>
2024-12-16 17:13:17 -0700

    cleaned up some code

--- signalNoiseRatio.py
+++ signalNoiseRatio.py
@@ -4,7 +4,6 @@
 from scipy.fftpack import fft
 from simulatedSignal import received_signal
 from simulatedSignal import t
-from simulatedSignal import fs
 from simulatedSignal import pulse
 from simulatedSignal import noise
 import numpy as np
